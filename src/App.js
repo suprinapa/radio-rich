ca7ba7e92881a689d7c7903dd56c78aef0ffa65e
suprina <suprinapa@gmail.com>
2024-03-27 21:14:20 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Radio from './Components/Radio';
+
+function App() {
+  return (
+    <div className="App">
+     <Radio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Radio.js
@@ -0,0 +1,13 @@
+const Radio = () =>{
+    return(
+        <div>
+            <h1>Radio Transcribing Website</h1>
+            <img src="/Static/Photos/Radio.png" alt="Radio Transcribing" className="centered-image" />
+            <button > Play </button> 
+            <button > Pause</button> 
+            <button > Transcribe </button> 
+        </div>
+    )
+}
+export default Radio;
+
